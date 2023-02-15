8db0ebcdb2d6a7e14c43a731154c3948a8835aaa
Emanuele D'Alessandro <dalessandroemanuelek@gmail.com>
2023-02-15 19:08:31 +0100

    faker funzionante

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    public function definition()
+    {
+        return [
+            'name' => fake()->words(3, true),
+            'description' => fake()->boolean()
+                ? fake()->paragraph()
+                : null,
+            'main_image' => fake()->imageUrl(640, 480, 'animals', true),
+            'release_date' => fake()->date(),
+            'repo_link' => fake()->url(),
+        ];
+    }
+}
---
+++ database/migrations/2023_02_15_175130_create_projects_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+
+            $table->id();
+
+            $table->string('name', 64)->unique();
+            $table->text('description')->nullable();
+            $table->string('main_image')->unique();
+            $table->date('release_date');
+            $table->string('repo_link')->unique();
+
+            $table->timestamps();
+        });
+    }
+};
